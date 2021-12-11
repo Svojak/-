8ef48a4031de36317c3c645de3fd1468ea9a5edb
Svojak <93858763+Svojak@users.noreply.github.com>
2021-12-11 17:27:57 +0700

    Add files via upload

---
+++ dz.c
@@ -0,0 +1,110 @@
+/*
+#include <stdio.h>
+int main()
+{
+    int a;
+scanf("%d", &a );
+    if(a > 0){
+        printf("+");
+        }
+   if(a < 0){
+    printf("-");
+   }
+   if(a==0){
+    printf("Z");
+   }
+   return 0;
+}
+*/
+
+
+
+/*
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a % 2==0){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
+
+*/
+
+/*
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if((a>=9 && a<=18)){
+       printf("DAY");
+    }
+
+    else{
+        printf("NIGHT");
+    }
+    return 0;
+}
+
+*/
+
+/*
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d", a/100);
+    return 0;
+
+}
+
+*/
+
+/*
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>c && a>b){
+            if(b>c){
+                printf("%d, %d, %d", c, b, a);
+            }
+        else
+            {printf("%d, %d, %d", b, c, a);
+        }
+
+    }
+
+    if(b>c && b>a){
+        if(a>c){
+            printf("%d, %d, %d", c, a, b);
+        }
+        else {printf("%d, %d, %d", a, c, b);
+
+        }
+    }
+    if(c>a && c>b){
+        if(b>a){
+            printf("%d, %d, %d", a, b, c);
+        }
+        else {
+            printf("%d, %d, %d", b, a, c);
+        }
+    }
+return 0;
+}
+*/
+
+
+
+
+
+
